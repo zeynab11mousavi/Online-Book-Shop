865807b42299304ca07f843e622d61d4f5e8f86d
Zeynab Mousavi <zeynab11mousavi.com>
2022-09-02 09:33:26 +0430

    UI : fix footer and navbar

--- src/Pages/Website/Navbar/Navbar.jsx
+++ src/Pages/Website/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import './Nav.css'
 import * as React from 'react'
+<<<<<<< Updated upstream
 import { AppBar, Typography, Toolbar, IconButton, Box } from '@mui/material'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from '../../../Assets/pic/logo.png'
 import { Button } from '@mui/material'
 import { ShoppingBasket } from '@mui/icons-material'
+=======
+import {
+  CardMedia,
+  AppBar,
+  Typography,
+  Toolbar,
+  IconButton,
+  Box,
+} from '@mui/material'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import logo from '../../../Assets/pic/logo.png'
+import { Button } from '@mui/material'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+>>>>>>> Stashed changes
 import Divider from '@mui/material/Divider'
 import Drawer from '@mui/material/Drawer'
 import List from '@mui/material/List'
@@ -12,6 +27,7 @@ import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import MenuIcon from '@mui/icons-material/Menu'
+<<<<<<< Updated upstream
 
 const drawerWidth = 240
 const navItems = [
@@ -28,6 +44,23 @@ const Navbar = (props) => {
   const { window } = props
   const [mobileOpen, setMobileOpen] = React.useState(false)
 
+=======
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchSubCategory } from '../../../Redux/subCategory/subCategory'
+import { useEffect } from 'react'
+const drawerWidth = 240
+
+const Navbar = (props) => {
+  const params = useParams()
+  const dispatch = useDispatch()
+  const { window } = props
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+  const { subCategories } = useSelector((state) => state.subCategories)
+
+  useEffect(() => {
+    dispatch(fetchSubCategory())
+  }, [])
+>>>>>>> Stashed changes
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen)
   }
@@ -39,10 +72,22 @@ const Navbar = (props) => {
       </Typography>
       <Divider />
       <List>
+<<<<<<< Updated upstream
         {navItems.map((item) => (
           <ListItem key={item} disablePadding>
             <ListItemButton sx={{ textAlign: 'center' }}>
               <ListItemText primary={item} />
+=======
+        {subCategories.map((item) => (
+          <ListItem key={item.id}>
+            <ListItemButton
+              key={item.id}
+              component={Link}
+              to={`${item.name}`}
+              sx={{ textAlign: 'center' }}
+            >
+              <ListItemText primary={item.name} />
+>>>>>>> Stashed changes
             </ListItemButton>
           </ListItem>
         ))}
@@ -50,6 +95,7 @@ const Navbar = (props) => {
     </Box>
   )
 
+<<<<<<< Updated upstream
   const container =
     window !== undefined ? () => window().document.body : undefined
   const navigate = useNavigate()
@@ -61,6 +107,23 @@ const Navbar = (props) => {
             src={logo}
             alt="logo"
             width={50}
+=======
+  const navigate = useNavigate()
+  const container =
+    window !== undefined ? () => window().document.body : undefined
+  // const handleAdminLoginAuth = () => {
+  //   isLoggedIn ? navigate('admin-panel') : navigate('admin-login')
+  // }
+  return (
+    <Box sx={{ display: 'flex', width: '100%' }}>
+      <AppBar color="info" component="nav">
+        <Toolbar>
+          <CardMedia
+            component="img"
+            image={logo}
+            alt="logo"
+            sx={{ width: { xs: '25px', lg: '45px', xl: '60px' } }}
+>>>>>>> Stashed changes
             onClick={() => navigate('/')}
             className="navigate-to-home"
           />
@@ -70,7 +133,11 @@ const Navbar = (props) => {
             variant="h3"
             color="primary"
             fontWeight="bold"
+<<<<<<< Updated upstream
             sx={{ ml: '5rem' }}
+=======
+            sx={{ fontSize: { xs: '14px', lg: '26px', xl: '36px' } }}
+>>>>>>> Stashed changes
           >
             ماتیکان
           </Typography>
@@ -79,29 +146,62 @@ const Navbar = (props) => {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
+<<<<<<< Updated upstream
             sx={{ mr: 2, display: { sm: 'none' } }}
+=======
+            sx={{ display: { sm: 'none' } }}
+>>>>>>> Stashed changes
           >
             <MenuIcon />
           </IconButton>
 
           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+<<<<<<< Updated upstream
             {navItems.map((item) => (
               <Button key={item}>{item}</Button>
+=======
+            {subCategories.map((item) => (
+              <Button key={item.id} component={Link} to={`${item.name}`}>
+                {item.name}
+              </Button>
+>>>>>>> Stashed changes
             ))}
           </Box>
 
           <Button
+<<<<<<< Updated upstream
             sx={{ mr: '3rem' }}
             variant="contained"
             component={Link}
             to="admin-login"
+=======
+            sx={{
+              width: { sm: '45px', lg: '70px', xl: '120px' },
+              fontSize: { sm: '8px', lg: '12px', xl: '14px' },
+              position: 'absolute',
+              left: '5rem',
+            }}
+            variant="contained"
+            component={Link}
+            to="/admin-login"
+>>>>>>> Stashed changes
             text="info"
           >
             پنل مدیریت
           </Button>
 
+<<<<<<< Updated upstream
           <IconButton color="primary" component={Link} to="shopping-basket">
             <ShoppingBasket />
+=======
+          <IconButton
+            color="primary"
+            sx={{ position: 'absolute', left: '2rem' }}
+            component={Link}
+            to="shopping-basket"
+          >
+            <ShoppingCartIcon />
+>>>>>>> Stashed changes
           </IconButton>
         </Toolbar>
       </AppBar>
